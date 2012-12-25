3e4de43b70d6830bf0fc6cdb4c5aa761045a6a08
Florian H <jscoderr@gmail.com>
2012-12-25 15:33:42 +0100

    init

---
+++ grunt.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+
+		meta: {
+			banner: '// ' + grunt.file.read('LICENSE').split("\n")[0]
+		},
+
+		coffee: {
+			compile: {
+				files: {
+					'lib/jquery.x18n.js': 'lib/jquery.x18n.coffee',
+					'spec/spec.js': 'spec/spec.coffee'
+				}
+			}
+		},
+
+		concat: {
+			dist: {
+				src: ['<banner>', 'lib/x18n/lib/x18n.js', 'lib/jquery.x18n.js'],
+				dest: 'lib/x18n.js'
+			}
+		},
+
+		mocha: {
+			all: {
+				src: 'spec/index.html',
+				run: true
+			}
+		},
+
+		watch: {
+			files: ['lib/jquery.x18n.coffee', 'spec/spec.coffee'],
+			tasks: 'coffee concat'
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-mocha');
+
+	grunt.registerTask('test', 'mocha');
+	grunt.registerTask('release', 'coffee concat');
+	grunt.registerTask('default', 'release');
+
+};
